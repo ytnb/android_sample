c25afbbf74eaa243bf0f0278a5718fc878d31154
ytnb <ytnbops@gmail.com>
2018-06-12 18:17:58 +0900

    fix kotlin

--- AnbayashiRoulette/app/src/main/java/com/example/ytnb/anbayashiroulette/AnbayasiAdapter.kt
+++ AnbayashiRoulette/app/src/main/java/com/example/ytnb/anbayashiroulette/AnbayasiAdapter.kt
@@ -4,10 +4,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 
-class AnbayasiAdapter(val roulette: ArrayList<AnbayasiData>) : RecyclerView.Adapter<AnbayasiViewHolder>() {
-    val rouletteDataSet: ArrayList<AnbayasiData> = roulette
+class AnbayasiAdapter(roulette: ArrayList<AnbayasiData>) : RecyclerView.Adapter<AnbayasiAdapter.AnbayasiViewHolder>() {
+    private val rouletteDataSet: ArrayList<AnbayasiData> = roulette
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnbayasiViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -16,15 +17,29 @@ class AnbayasiAdapter(val roulette: ArrayList<AnbayasiData>) : RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: AnbayasiViewHolder, listPosition: Int) {
-        holder.textViewNumber.text = "${rouletteDataSet[listPosition].number}本"
+//        holder.textViewNumber.text = "${rouletteDataSet[listPosition].number} 本"
+        holder.textViewNumber.text = holder.base.context.getString(
+                R.string.view_number,
+                rouletteDataSet[listPosition].number
+        )
         holder.textViewComment.text = rouletteDataSet[listPosition].comment
-        holder.base.setOnClickListener(View.OnClickListener {
-            Toast.makeText(it.context, "おまけ${rouletteDataSet[listPosition].addition}本", Toast.LENGTH_SHORT).show()
+        holder.base.setOnClickListener({
+            Toast.makeText(
+                    it.context,
+                    "おまけ${rouletteDataSet[listPosition].addition}本",
+                    Toast.LENGTH_SHORT
+            ).show()
         })
 
     }
 
     override fun getItemCount(): Int {
         return rouletteDataSet.size
+    }
+
+    class AnbayasiViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+        val base: View = v
+        val textViewNumber: TextView = v.findViewById(R.id.number2)
+        val textViewComment: TextView = v.findViewById(R.id.comment2)
     }
 }
--- MultiActivity/app/src/main/java/com/example/ytnb/multiactivity/FirstActivity.kt
+++ MultiActivity/app/src/main/java/com/example/ytnb/multiactivity/FirstActivity.kt
@@ -3,7 +3,7 @@ package com.example.ytnb.multiactivity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
 
@@ -11,8 +11,7 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
 
-        val btnNext: Button = findViewById(R.id.button1)
-        btnNext.setOnClickListener {
+        button1.setOnClickListener {
                 val intent = Intent(this, SecondActivity::class.java)
                 startActivity(intent)
         }
